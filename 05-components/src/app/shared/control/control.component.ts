6c81297cbf11a4b9834ae76e52f26239432724d0
Satyajit Shetye <sshetye@Satyajits-MacBook-Pro.local>
2024-08-01 15:55:10 +0530

    practice components

---
+++ 05-components/src/app/shared/control/control.component.ts
@@ -0,0 +1,22 @@
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  ViewEncapsulation,
+  input,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+  },
+})
+export class ControlComponent {
+  label = input.required<string>();
+}
